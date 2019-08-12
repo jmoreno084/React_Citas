f1caa56636fa97beb00d52403d5a6709e5e39b25
Jaime Moreno <jmoreno084@gmail.com>
2019-08-11 23:55:11 -0500

    generación de funciones para eliminado de cita y almacenamiento en LocalStorage

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, {useState, Fragment} from 'react';
+import React, {useState, useEffect, Fragment} from 'react';
 
-function Cita({cita}) {
+function Cita({cita, index, eliminarCita}) {
   return (
     <div className="cita">
       <p>Mascota: <span>{cita.mascota}</span></p> 
@@ -8,20 +8,29 @@ function Cita({cita}) {
       <p>Fecha: <span>{cita.fecha}</span></p>
       <p>Hora: <span>{cita.hora}</span></p>
       <p>Sintomas: <span>{cita.sintomas}</span></p>
+      <button 
+        onClick={()=>eliminarCita(index)}
+        type="button" className="button eliminar u-full-width">Eliminar X</button>
     </div>
   );
 }
 
 
 function Formulario({crearCita}) {
-  const [cita, actualizarCita] = useState({
+
+  const stateInicial = {
     mascota: '',
     propietario: '',
     fecha: '',
     hora: '',
     sintomas: ''
-  });
+  }
 
+  // cita= state actual
+  // actualizarCita = fn para cambiar el state
+  const [cita, actualizarCita] = useState(stateInicial);
+
+  // actualiza el state
   const actualizarState = (e) => {
     actualizarCita({
       ...cita,
@@ -29,13 +38,14 @@ function Formulario({crearCita}) {
     });
   };
 
-
+  // pasamos la cita al componente principal
   const enviarCita = (e) => {
     e.preventDefault();
 
     // pasar la cita al componente principal
     crearCita(cita);
     // reiniciar el state (el form)
+    actualizarCita(stateInicial);
   };
 
   return (
@@ -96,10 +106,15 @@ function Formulario({crearCita}) {
 }
 
 function App() {
+  // cargar las citas al localStorage como state inicial
+  let citasIniciales = JSON.parse(localStorage.getItem('citas'));
+  if(!citasIniciales)
+    citasIniciales = [];
+
   // haciendo uso se useState
   // 1. el state actual = this.state
   // 2. funcion que actualiza el state: this.setState({})
-  const [citas, guardarCita] = useState([]);
+  const [citas, guardarCita] = useState(citasIniciales);
 
   const crearCita = cita => {
     // tomar copia del state y crear nueva cita
@@ -107,6 +122,27 @@ function App() {
     // almacenamos en el state
     guardarCita(nuevasCitas);
   }
+
+  // eliminamos la cita del state
+  const eliminarCita = index => {
+    const nuevasCitas =  [...citas];
+    nuevasCitas.splice(index, 1);
+    guardarCita(nuevasCitas);
+  }
+
+  useEffect(
+    ()=>{
+      let citasIniciales = JSON.parse(localStorage.getItem('citas'));
+      if(citasIniciales) 
+        localStorage.setItem('citas', JSON.stringify(citas));
+      else 
+        localStorage.setItem('citas', JSON.stringify([]));
+    }, [citas]  // se controla que solo se ejecute cuando citas tengan algún cambio
+  )
+
+  // mensaje dinámico de administración de citas
+  const titulo = Object.keys(citas).length === 0 ? 'No hay citas' : 'Administrar las citas aqui';
+
 
   return (
     <Fragment>
@@ -120,11 +156,13 @@ function App() {
              />
            </div>        
            <div className="one-half column">
+             <h2>{titulo}</h2>
             {citas.map((cita, index)=>(
               <Cita 
                 key={index}
                 index={index}
                 cita={cita}
+                eliminarCita={eliminarCita}
               />
             ))} 
             </div>        
